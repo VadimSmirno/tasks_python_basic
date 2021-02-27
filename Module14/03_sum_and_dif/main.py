f5d5089565349d6aa4e415bbf07f6a654a37d790
likhacheva-s <Kapitoxa93!>
2021-02-27 14:46:40 +0300

    разность суммы и количества цифр

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+n = int(input('Введите число: '))
+def summ_numeral (n):
+    summ = 0
+    while n > 0:
+        summ += n%10
+        n //= 10
+    print ('Сумма цифр:', summ)
+    return summ
+def count_numeral (n):
+    count = 0
+    while n > 0:
+        n//= 10
+        count +=1
+    print ('Количество цифр:', count)
+    return count
+
+print ('Разность суммы  и количества цифр:', summ_numeral(n) - count_numeral(n))
+
+
