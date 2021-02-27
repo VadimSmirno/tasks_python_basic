b39157789ee4a9dc1e20fa4bf1da81cdce851c0b
likhacheva-s <Kapitoxa93!>
2021-02-27 19:15:20 +0300

    сумма измененных чисел

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,39 @@
-# TODO здесь писать код
+
+
+def reverse_number(n,NUM):
+    fractional_part = ''
+    whole_part = ''
+    new_number = ''
+
+    for symbol in  (n):
+        new_number = symbol + new_number
+
+    for symbol in (n):
+        if symbol != '.':
+            whole_part = symbol + whole_part
+        else:
+            break
+    for symbol in (new_number):
+        if symbol != '.':
+            fractional_part = fractional_part +symbol
+        else:
+            break
+
+    a = whole_part +'.'+fractional_part
+
+
+
+    print (NUM,'число наоборот:',a)
+    return  float (a)
+
+
+
+
+
+
+n = input ('Введите первое число ')
+
+n2 =  input ('Введите второе число ')
+print (reverse_number(n,'Первое') + reverse_number( n2,'Второе'))
 
 
