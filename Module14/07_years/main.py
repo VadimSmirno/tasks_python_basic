7323d3c68abb779d0cc78df9e4662acae192f8a8
likhacheva-s <Kapitoxa93!>
2021-02-28 10:56:15 +0300

    задача "годы"

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+first_year = int (input('Введите первый год '))
+second_year = int (input('Введите второй год '))
+
+for year in range (first_year,second_year):
+    a = year%10
+    b = year%100//10
+    c = year//100%10
+    d = year//1000
+    if a == b == c or b == c == d or a == d == b:
+        print (year)
