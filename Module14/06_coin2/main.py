074bddbd025542070015a03b115c5065475fb6ae
likhacheva-s <Kapitoxa93!>
2021-02-28 17:58:28 +0300

    Реализовал функцию. "принт" перенес на новую строку

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,9 +1,11 @@
-print ('Введите координаты монетки')
-x = float (input('X: '))
-y = float (input('Y: '))
-radius = float (input('Введите радиус '))
-if x < radius and y < radius: print ('Монетка где-то рядом')
-else: print ('Монетки в области нет')  # TODO, исходя из PEP8, print должен быть с новой строки =)
+def coin ():
+    print ('Введите координаты монетки')
+    x = float (input('X: '))
+    y = float (input('Y: '))
+    radius = float (input('Введите радиус '))
+    if x < radius and y < radius:
+        print ('Монетка где-то рядом')
+    else:
+        print ('Монетки в области нет')
+coin ()
 
-
-# TODO, пожалуйста, реализуйте функцией.
