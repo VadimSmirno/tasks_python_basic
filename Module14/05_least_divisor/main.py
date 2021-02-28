fd1a6abf41128cbb24091d91988a06e15ae37662
Insolente13 <lanc05@mail.ru>
2021-02-28 19:09:10 +0300

    Module 14 ok

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,12 +1,11 @@
-
-
-def divider (number):
+def divider(number):
     for numeral in range(2, number + 1):
         if number % numeral == 0:
             print('Наименьший делитель ', numeral)
             break
+
+
 number = int(input('Введите число '))
-divider (number)
+divider(number)
 
-
-
+# зачёт!
--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,11 +1,14 @@
-def coin ():
-    print ('Введите координаты монетки')
-    x = float (input('X: '))
-    y = float (input('Y: '))
-    radius = float (input('Введите радиус '))
+def coin():
+    print('Введите координаты монетки')
+    x = float(input('X: '))
+    y = float(input('Y: '))
+    radius = float(input('Введите радиус '))
     if x < radius and y < radius:
-        print ('Монетка где-то рядом')
+        print('Монетка где-то рядом')
     else:
-        print ('Монетки в области нет')
-coin ()
+        print('Монетки в области нет')
 
+
+coin()
+
+# зачёт!
