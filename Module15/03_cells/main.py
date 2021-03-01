d66d8a23b757376a52d937a607830c2b3c961d6e
likhacheva-s <Kapitoxa93!>
2021-03-01 13:12:20 +0300

    Задача на нахождение нужных клеток

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+cell = int (input ('Количество клеток: '))
+list_efficiency_cell = []
+for num in range (1,cell+1):
+    print ('эффективность ', num, ' клетки: ', end = '')
+    efficiency_cell = int (input ())
+    if num > efficiency_cell:
+      list_efficiency_cell.append(efficiency_cell)
+
+print ('Неподходящие значения: ', end = '')
+for index in (list_efficiency_cell):
+    print ( index, end = ' ')
