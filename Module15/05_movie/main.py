b744654f1a093bc604a5e9a11a210bcc25b46baf
likhacheva-s <Kapitoxa93!>
2021-03-01 17:53:57 +0300

    Любимые фильмы

--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -1,5 +1,18 @@
 films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
          'Леон', 'Богемская рапсодия', 'Город грехов',
          'Мементо', 'Отступники', 'Деревня']
+count_films = len (films)
 
-# TODO здесь писать код
+favorite_films = []
+while count_films != 0:
+    interested_films = input('Введите название фильма: ')
+    count_films -= 1
+
+    if interested_films in films:
+        favorite_films.append(interested_films)
+    else:
+        print('Ошибка, такого фильма на сайте нет')
+
+
+print ('Любимые фильмы', favorite_films)
+
