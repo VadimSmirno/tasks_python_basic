27c47649c0942cee62881e7c80c5234733b739dc
likhacheva-s <Kapitoxa93!>
2021-03-01 12:30:34 +0300

    список нечетных чисел

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+number = int(input('Введите число: '))
+list_number = []
+for num in range (1, number +1, 2):
+    list_number.append(num)
+print (list_number)
+print (len (list_number))
