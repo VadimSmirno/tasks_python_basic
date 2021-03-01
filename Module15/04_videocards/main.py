9815b9c8d0f9a05e68221029221463ff625561fb
likhacheva-s <Kapitoxa93!>
2021-03-01 16:53:14 +0300

    Видеокарты

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,22 @@
-# TODO здесь писать код
+number_video_cards = int(input('Введите количество видеокарт: '))
+old_list_video_cards = []
+new_list_video_cards = []
+
+for index in range (1,number_video_cards+1):
+
+    print ('Видеокарта №', index, ':', end = '')
+    version_video_cards = int(input())
+    old_list_video_cards.append(version_video_cards)
+
+for num in  old_list_video_cards:
+    if num < max(old_list_video_cards):
+        new_list_video_cards.append(num)
+
+
+
+
+
+print('Старый список видеокарт: ',old_list_video_cards )
+print('Новый список видеокарт: ',new_list_video_cards )
 
 
