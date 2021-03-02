a5e431aed165b87eadd2b86b4c534d2ece58d89f
Insolente13 <lanc05@mail.ru>
2021-03-02 23:58:02 +0300

    Module 15 05 - 06 - TODO, another - ok

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,4 +1,4 @@
-def max_num (old_list_video_cards):
+def max_num(old_list_video_cards):
     max_num = 0
     for index in old_list_video_cards:
         if index > max_num:
@@ -10,21 +10,17 @@ number_video_cards = int(input('Введите количество видеок
 old_list_video_cards = []
 new_list_video_cards = []
 
-for index in range (1,number_video_cards+1):
-    print ('Видеокарта №', index, ':', end = '')
+for index in range(1, number_video_cards + 1):
+    print('Видеокарта №', index, ':', end='')
     version_video_cards = int(input())
     old_list_video_cards.append(version_video_cards)
 
-for num in  old_list_video_cards:
+for num in old_list_video_cards:
 
-    if num < max_num (old_list_video_cards) :
+    if num < max_num(old_list_video_cards):
         new_list_video_cards.append(num)
 
+print('Старый список видеокарт: ', old_list_video_cards)
+print('Новый список видеокарт: ', new_list_video_cards)
 
-
-
-
-print('Старый список видеокарт: ',old_list_video_cards )
-print('Новый список видеокарт: ',new_list_video_cards )
-
-
+# зачёт!
--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -1,19 +1,21 @@
 films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
          'Леон', 'Богемская рапсодия', 'Город грехов',
          'Мементо', 'Отступники', 'Деревня']
-count_films = len (films)
+count_films = len(films)
 
 favorite_films = []
 
-for _ in range (1,len(films)+1):
+# TODO, "while count_films != 0:" Правильно проверять "0", таким образом проверяем текст, проверяя 0 проверяем число =)
+#  К сожалению, для python это 2 разных символа. Предлагаю попробовать реализовать
+#  цикл while с возможностью выхода из цикла =)
+for _ in range(1, len(films) + 1):
     interested_films = input('Введите название фильма: ')
-
 
     if interested_films in films:
         favorite_films.append(interested_films)
     else:
         print('Ошибка, такого фильма на сайте нет')
 
+print('Любимые фильмы', favorite_films)
 
-print ('Любимые фильмы', favorite_films)
 
