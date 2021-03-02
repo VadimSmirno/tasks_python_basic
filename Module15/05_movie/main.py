ee84e2c3a15b169855d18c7ddd67858bd4a7f837
Insolente13 <lanc05@mail.ru>
2021-03-03 01:41:34 +0300

    Module 15 05 - TODO, another - ok

--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -5,9 +5,10 @@ count_films = len(films)
 
 favorite_films = []
 
-
+# TODO, таким образом первый фильм не добавляется в список любимы, если есть в основном списке =)
 interested_films = input('Введите название фильма: ')
 while interested_films != 'end':
+    # TODO, возможно этот запрос стоит реализовать после проверки.
     interested_films = input('Введите название фильма: ')
 
     if interested_films in films:
--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,8 +1,7 @@
-
 word = input('Введите слово:')
-word_list = list (word)
+word_list = list(word)
 count = 0
-new_word_list =[]
+new_word_list = []
 num = 0
 
 for i_mn in range(len(word_list)):
@@ -11,6 +10,6 @@ for i_mn in range(len(word_list)):
         if word_list[i_mn] == word_list[curr]:
             count += 1
 
-print ('Количество уникальных букв', len(word_list)-count )
+print('Количество уникальных букв', len(word_list) - count)
 
-
+# зачёт!
