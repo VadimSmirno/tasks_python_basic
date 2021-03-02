69fc3867e1e717d5e901a047583b0317fc29be46
likhacheva-s <Kapitoxa93!>
2021-03-02 12:26:39 +0300

    Уникальные буквы

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+
+word = input('Введите слово:')
+word_list = list (word)
+count = 0
+new_word_list =[]
+
+for index in word_list:
+    if index not in new_word_list:
+        new_word_list.append(index)
+        count+=1
+
+
+print ('Количество уникальных букв', count )
+print ('Уникальные буквы', new_word_list)
