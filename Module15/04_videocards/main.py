38897b55bf6bc398cc218670bf593a77ae8fedb7
likhacheva-s <Kapitoxa93!>
2021-03-02 22:22:44 +0300

    Написал функцию нахождения максимального значения в списке

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,16 +1,23 @@
+def max_num (old_list_video_cards):
+    max_num = 0
+    for index in old_list_video_cards:
+        if index > max_num:
+            max_num = index
+    return max_num
+
+
 number_video_cards = int(input('Введите количество видеокарт: '))
 old_list_video_cards = []
 new_list_video_cards = []
 
 for index in range (1,number_video_cards+1):
-
     print ('Видеокарта №', index, ':', end = '')
     version_video_cards = int(input())
     old_list_video_cards.append(version_video_cards)
 
 for num in  old_list_video_cards:
-    # TODO, как нам определить максимальное число без функции max? =)
-    if num < max(old_list_video_cards):
+
+    if num < max_num (old_list_video_cards) :
         new_list_video_cards.append(num)
 
 
