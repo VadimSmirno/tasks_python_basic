5e79ddade0a0808504ce9fd1d7de108eaf1a6fce
likhacheva-s <Kapitoxa93!>
2021-03-02 14:21:57 +0300

    Сортировка списка

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def selection_sort (my_list):
+    for i_mn in range (len(my_list)):
+        for curr in range (i_mn,len(my_list)):
+            if my_list[curr] < my_list[i_mn]:
+                my_list[curr], my_list[i_mn] = my_list[i_mn], my_list[curr]
+
+
+nums = [5,6,98,9,4,1]
+
+selection_sort(nums)
+
+print (nums)
