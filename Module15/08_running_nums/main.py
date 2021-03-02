38d0fa93a1e99a75f3e97227db30e11b3bb9d8a2
likhacheva-s <Kapitoxa93!>
2021-03-02 14:05:34 +0300

    Сдвинутый список

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+start_list = [1,2,3,4,5]
+shift = int(input('Сдвиг: '))
+finish_list =  []
+
+for i in range (1,shift+1):
+    finish_list.append(start_list[len(start_list)-i])
+
+for index in start_list:
+    finish_list.append(index)
+
+
+finish_list=finish_list[:-shift]
+print ('Изначальный список', start_list)
+print ('Сдвинутый список',finish_list)
