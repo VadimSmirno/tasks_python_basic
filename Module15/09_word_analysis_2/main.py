53de010b9af65cbb72d694e4609e45c6b48dfc84
likhacheva-s <Kapitoxa93!>
2021-03-02 18:26:48 +0300

    Слова - палиндромы

---
+++ Module15/09_word_analysis_2/main.py
@@ -0,0 +1,15 @@
+def comparison (word_list):
+    count = 0
+    for i_mn in range (len(word_list)):
+        if word_list[i_mn] == word_list[-(i_mn+1)]:
+            count += 1
+    if count == len((word_list)):
+        print ('Слово является палиндромом')
+    else:
+        print( 'Слово не является палиндромом')
+
+
+word = input('Введите слово: ')
+word_list = list(word)
+
+comparison (word_list)
