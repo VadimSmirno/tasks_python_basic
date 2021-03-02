e70b99956a3219794ce1940f206f2e31dd46ea47
likhacheva-s <Kapitoxa93!>
2021-03-03 00:05:43 +0300

    В карамба теперь 4 уникальные буквы=))

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -3,18 +3,14 @@ word = input('Введите слово:')
 word_list = list (word)
 count = 0
 new_word_list =[]
+num = 0
 
-for index in word_list:
-    if index not in new_word_list:
-        new_word_list.append(index)
-        count+=1
+for i_mn in range(len(word_list)):
+    num += 1
+    for curr in range(num, len(word_list)):
+        if word_list[i_mn] == word_list[curr]:
+            count += 1
+
+print ('Количество уникальных букв', len(word_list)-count )
 
 
-print ('Количество уникальных букв', count )
-print ('Уникальные буквы', new_word_list)
-
-# TODO
-#  Введите слово:карамба
-#  Количество уникальных букв 5
-#  Уникальные буквы ['к', 'а', 'р', 'м', 'б']
-#  По идее, буква "а" не уникальна =)
