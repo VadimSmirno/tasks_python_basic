f4bc2c60678819cca55e6f8042d3a4fa10135192
likhacheva-s <Kapitoxa93!>
2021-03-03 10:16:14 +0300

    Изменил цикл

--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -5,16 +5,14 @@ count_films = len(films)
 
 favorite_films = []
 
-# TODO, таким образом первый фильм не добавляется в список любимы, если есть в основном списке =)
+
 interested_films = input('Введите название фильма: ')
 while interested_films != 'end':
-    # TODO, возможно этот запрос стоит реализовать после проверки.
-    interested_films = input('Введите название фильма: ')
-
     if interested_films in films:
         favorite_films.append(interested_films)
     else:
         print('Ошибка, такого фильма на сайте нет')
+    interested_films = input('Введите название фильма: ')
 
 print('Любимые фильмы', favorite_films)
 
