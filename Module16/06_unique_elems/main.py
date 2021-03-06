173902e8f09ba713a8c0e2f9b3a32eefb8c744c3
Insolente13 <lanc05@mail.ru>
2021-03-06 12:02:17 +0300

    Module 16 06 - ok

--- Module16/06_unique_elems/main.py
+++ Module16/06_unique_elems/main.py
@@ -1,13 +1,12 @@
-
 first_list = []
 second_list = []
-for i_fl in range (1,4):
-    print('Введите ',i_fl,'-е  число в первый список: ', end = '')
+for i_fl in range(1, 4):
+    print('Введите ', i_fl, '-е  число в первый список: ', end='')
     number = int(input())
     first_list.append(number)
-print ()
-for i_sl in range (1,8):
-    print('Введите ',i_sl,'-е  число во второй список: ', end = '')
+print()
+for i_sl in range(1, 8):
+    print('Введите ', i_sl, '-е  число во второй список: ', end='')
     number = int(input())
     second_list.append(number)
 
@@ -15,8 +14,9 @@ new_list = []
 new_list.extend(first_list)
 new_list.extend((second_list))
 
-
 for index in new_list:
     while new_list.count(index) > 1:
         new_list.remove(index)
-print (new_list)
+print(new_list)
+
+# зачёт!
