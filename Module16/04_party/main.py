a2f79abf92650ea719b93fd47f73350f14163e42
Insolente13 <lanc05@mail.ru>
2021-03-06 12:00:24 +0300

    Module 16 04 - ok

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,7 +1,6 @@
 guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
 
-
-ask = input ('Гость пришел или ушел? ')
+ask = input('Гость пришел или ушел? ')
 while ask != 'Пора спать':
     name_guest = input('Имя гостя: ')
     if ask == 'пришел' and len(guests) < 6:
@@ -10,10 +9,12 @@ while ask != 'Пора спать':
     elif ask == 'ушел':
         if name_guest in guests:
             guests.remove(name_guest)
-            print ('Пока', name_guest)
+            print('Пока', name_guest)
         else:
-            print ('Человека с именем',name_guest,'нет в гостях')
+            print('Человека с именем', name_guest, 'нет в гостях')
     else:
-        print('Прости,', name_guest,', но мест нет')
+        print('Прости,', name_guest, ', но мест нет')
     print('Сейчас на вечеринке', len(guests), ' человек', guests)
     ask = input('Гость пришел или ушел? ')
+
+# зачёт!
