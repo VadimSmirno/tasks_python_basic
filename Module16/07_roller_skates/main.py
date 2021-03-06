e0645e295efc4a4fb58c1c9c2c90a533f860122f
Insolente13 <lanc05@mail.ru>
2021-03-06 12:04:36 +0300

    Module 16 07 - TODO

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -2,18 +2,22 @@ skate_size_list = []
 foot_size_list = []
 count = 0
 number_skates = int(input('Количество коньков: '))
-for i_ns in range(1, number_skates +1):
-    print('размер ',i_ns,'пары: ', end = '')
+for i_ns in range(1, number_skates + 1):
+    print('размер ', i_ns, 'пары: ', end='')
     skate_size = int(input())
     skate_size_list.append(skate_size)
 
 number_people = int(input('\nКоличество людей: '))
-for i_np in range (1,number_people+1):
-    print ('Размер ноги', i_np,'человека: ', end = '')
+for i_np in range(1, number_people + 1):
+    print('Размер ноги', i_np, 'человека: ', end='')
     foot_size = int(input())
     foot_size_list.append(foot_size)
 
-for _ in range (number_skates):
+# TODO, предлагаю упростить решение. Не использовать функцию min.
+#  Можно идти по списку с размерами людей в цикле и проверять наличие размера в списке с размерами коньков.
+#  Если есть, к счётчику +1 и удаляем 1 элемент из списка с размерами коньков.
+
+for _ in range(number_skates):
     if min(foot_size_list) <= min(skate_size_list):
         count += 1
         foot_size_list.remove(min(foot_size_list))
@@ -21,6 +25,4 @@ for _ in range (number_skates):
     elif min(foot_size_list) > min(skate_size_list):
         skate_size_list.remove((min(skate_size_list)))
 
-
-
-print ('Наибольшее количество людей которые могут взять ролики:', count)
+print('Наибольшее количество людей которые могут взять ролики:', count)
