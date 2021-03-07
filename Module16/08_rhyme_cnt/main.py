c14ec5271b0ddc6eab8d30921688cd910ebcae04
likhacheva-s <Kapitoxa93!>
2021-03-07 18:59:42 +0300

    Задача 8 исправил цикл

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -7,17 +7,11 @@ people = []
 
 
 people.extend(list(range(1, number_people + 1)))
-# TODO, по идее, эти вычисления должны быть тоже в цикле =)
-#  Возможно будет удобнее использовать цикл while с условием.
-list_length = number % len(people)
-print('Текущий круг людей: ', people)
-print('Начало счета c номера 1')
-print('Выбывает человек под номером: ', people[list_length - 1])
 
-for index in range(number_people - 1):
+while len(people) != 1:
     list_length = number % len(people)
     print('\nТекущий круг людей: ', people)
-    print('Начало счета c номера 1')
+    print('Начало счета c номера',people[list_length - 2] )
     print('Выбывает человек под номером: ', people[list_length - 1])
     people.remove(people[list_length - 1])
 
