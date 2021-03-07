f3c2dfa5751515c98bdc11563b46fd9bf42f551e
likhacheva-s <Kapitoxa93!>
2021-03-07 16:50:18 +0300

    Задача 8 программа работаеи неправильно

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+
+number_people = int(input('Количество человек: '))
+number = int(input('Какое число в считалке: '))
+print ('\nЗначит выбывает каждый ', number, 'человек')
+
+people = []
+people.extend(list(range(1,number_people+1)))
+list_length = number % len(people)
+print('Текущий круг людей: ', people)
+print ('Начало счета c номера 1' )
+print ('Выбывает человек под номером: ',people[list_length-1])
+
+for index in range (number_people -1):
+    list_length = number % len(people)
+    print('\nТекущий круг людей: ', people)
+    print ('Начало счета c номера 1' )
+    print ('Выбывает человек под номером: ',people[list_length-1])
+    people.remove(people[list_length-1])
+
+print ('\nОстался человек под номером', people)
+
+
