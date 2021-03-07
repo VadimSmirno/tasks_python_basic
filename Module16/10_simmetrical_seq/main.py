6d63222d4e48690b05c027a5a1d33f1d8c5901f8
Insolente13 <lanc05@mail.ru>
2021-03-07 18:14:31 +0300

    Module 16 10 - ok

--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,21 +1,22 @@
-def is_palindrome (num_list):
+def is_palindrome(num_list):
     reverse_list = []
-    for i_num in range(len(num_list)-1,-1,-1):
+    for i_num in range(len(num_list) - 1, -1, -1):
         reverse_list.append(num_list[i_num])
     if num_list == reverse_list:
-        return  True
+        return True
     else:
         return False
 
-nums = [1,2,3,3]
+
+nums = [1, 2, 3, 3, 5]
 new_nums = []
 answer = []
 
-for i_nums in range (0, len(nums)):
-    for j_elem in range (i_nums,len(nums)):
+for i_nums in range(0, len(nums)):
+    for j_elem in range(i_nums, len(nums)):
         new_nums.append(nums[j_elem])
     if is_palindrome(new_nums):
-        for i_answer in range(0,i_nums):
+        for i_answer in range(0, i_nums):
             answer.append(nums[i_answer])
         answer.reverse()
         break
@@ -23,4 +24,6 @@ for i_nums in range (0, len(nums)):
 
 print('Исходный список:', nums)
 print('Нужно чисел для палиндрома:', len(answer))
-print( 'Список этик чисел:', answer)
+print('Список этик чисел:', answer)
+
+# зачёт!
