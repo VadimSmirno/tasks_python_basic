12dbeb5906e4d9d3aa1aeb441224e3a948f53ff7
Insolente13 <lanc05@mail.ru>
2021-03-07 18:13:34 +0300

    Module 16 08 - TODO

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,22 +1,24 @@
-
 number_people = int(input('Количество человек: '))
 number = int(input('Какое число в считалке: '))
-print ('\nЗначит выбывает каждый ', number, 'человек')
+print('\nЗначит выбывает каждый ', number, 'человек')
+
 
 people = []
-people.extend(list(range(1,number_people+1)))
+
+
+people.extend(list(range(1, number_people + 1)))
+# TODO, по идее, эти вычисления должны быть тоже в цикле =)
+#  Возможно будет удобнее использовать цикл while с условием.
 list_length = number % len(people)
 print('Текущий круг людей: ', people)
-print ('Начало счета c номера 1' )
-print ('Выбывает человек под номером: ',people[list_length-1])
+print('Начало счета c номера 1')
+print('Выбывает человек под номером: ', people[list_length - 1])
 
-for index in range (number_people -1):
+for index in range(number_people - 1):
     list_length = number % len(people)
     print('\nТекущий круг людей: ', people)
-    print ('Начало счета c номера 1' )
-    print ('Выбывает человек под номером: ',people[list_length-1])
-    people.remove(people[list_length-1])
+    print('Начало счета c номера 1')
+    print('Выбывает человек под номером: ', people[list_length - 1])
+    people.remove(people[list_length - 1])
 
-print ('\nОстался человек под номером', people)
-
-
+print('\nОстался человек под номером', people)
