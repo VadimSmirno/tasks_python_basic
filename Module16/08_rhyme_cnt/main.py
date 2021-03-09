0ac2fe9d67e3acc1b4c0bcf66e107bcd1d3cc891
likhacheva-s <Kapitoxa93!>
2021-03-09 16:53:19 +0300

    Задача 8 не знаю как использовать доп.переменную

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -14,12 +14,13 @@ people.extend(list(range(1, number_people + 1)))
 #  Предлагаю создать дополнительную переменную с изначальным значением равным "0".
 #  Возможно её же можно будет использовать для расчёта индекса для удаления.
 # TODO, предлагаю создать дополнительную переменную для расчёта индекса элемента, который необходимо удалить.
-
+count = 0
 while len(people) != 1:
     list_length = number % len(people)
     print('\nТекущий круг людей: ', people)
-    print('Начало счета c номера',people[list_length - 2] )
-    print('Выбывает человек под номером: ', people[list_length - 1])
-    people.remove(people[list_length - 1])
+    print('Начало счета c номера',people[count] )
+    print('Выбывает человек под номером: ', people[count])
+    count += 1
+    people.remove(people[count])
 
 print('\nОстался человек под номером', people)
