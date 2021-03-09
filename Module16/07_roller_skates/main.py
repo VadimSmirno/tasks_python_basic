a6d3f811325ffafd0913f5af9224f4aec0a55472
Insolente13 <lanc05@mail.ru>
2021-03-10 00:31:24 +0300

    Module 16 07 - ok

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -3,7 +3,6 @@ foot_size_list = []
 count = 0
 number_skates = int(input('Количество коньков: '))
 
-
 for i_number_skates in range(1, number_skates + 1):
     print('размер ', i_number_skates, 'пары: ', end='')
     skate_size = int(input())
@@ -15,10 +14,11 @@ for i_number_people in range(1, number_people + 1):
     foot_size = int(input())
     foot_size_list.append(foot_size)
 
-
 for index in foot_size_list:
     if index in skate_size_list:
         count += 1
         skate_size_list.remove(index)
 
 print('Наибольшее количество людей которые могут взять коньки:', count)
+
+# зачёт!
