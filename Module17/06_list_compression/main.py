c37acc8855fef4b274c3453b92011ef28c0a5334
likhacheva-s <Kapitoxa93!>
2021-03-13 20:55:03 +0300

    Задача 6

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+import  random
+number = int(input('Сколько чисел будет? '))
+number_list = [ random.randint(0,5) for _ in range(number)]
+count_0 = number_list.count(0)
+new_list = [index for index in reversed((sorted(number_list)))]
+print ('Сжатый список: ',new_list)
+print ('Список без "0" : ', new_list[:-count_0])
