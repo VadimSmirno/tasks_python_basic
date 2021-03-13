7aed343863c108b37c7d0b323f6ecf46ff3bdb9d
likhacheva-s <Kapitoxa93!>
2021-03-13 18:57:55 +0300

    Задача 1

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,4 @@
-# TODO здесь писать код
+text = input('Введите текст: ')
+vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
+vowels_text = [index for index in text if index in vowels]
+print ('Список гласных букв: ',vowels_text, '\nДлина списка: ', len(vowels_text))
