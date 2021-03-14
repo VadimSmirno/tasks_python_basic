7ca9452523d23836302fa378edef95e7264d58c2
likhacheva-s <Kapitoxa93!>
2021-03-14 12:48:31 +0300

    Задача 9

--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,4 +1,7 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+new_list = [i_third for i_first in nice_list
+            for i_second in i_first
+            for i_third in i_second]
+print (new_list)
