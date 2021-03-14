7f31ba6ea826fec95109605cff3cbb8a837e7538
Insolente13 <lanc05@mail.ru>
2021-03-14 16:54:11 +0300

    Module 17 03 - ok

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,10 +1,11 @@
 import random
 
-
-first_team = [round(random.uniform(5,10), 2) for _ in range (20)]
-second_team = [round(random.uniform(5,10), 2) for _ in range (20)]
+first_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
+second_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
 winners = [first_team[index] if first_team[index] > second_team[index]
            else second_team[index] for index in range(20)]
 print('Первая команда: ', first_team)
-print ('Вторая команда: ', second_team)
-print ('Победители турнира: ',winners)
+print('Вторая команда: ', second_team)
+print('Победители турнира: ', winners)
+
+# зачёт!
