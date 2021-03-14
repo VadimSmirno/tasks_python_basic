e7b252505b6637dad4d49eb0644b0a36ae3286a0
Insolente13 <lanc05@mail.ru>
2021-03-14 16:57:41 +0300

    Module 17 08 - ok

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -2,14 +2,16 @@ number_of_sticks = int(input('Количество палок: '))
 throw = int(input('Количество бросков: '))
 
 list_sticks = []
-for _ in range (number_of_sticks):
+for _ in range(number_of_sticks):
     list_sticks.append('|')
-for index in range (1, throw + 1):
-    print('Бросок ', index,'. Сбиты палки с номера ', end = '')
+for index in range(1, throw + 1):
+    print('Бросок ', index, '. Сбиты палки с номера ', end='')
     start_sticks = int(input())
-    print ('по номер ', end = '')
+    print('по номер ', end='')
     stop_sticks = int(input())
-    list_sticks [start_sticks-1:stop_sticks] = ['.']*(stop_sticks-start_sticks+1)
+    list_sticks[start_sticks - 1:stop_sticks] = ['.'] * (stop_sticks - start_sticks + 1)
 
 for i_list_sticks in list_sticks:
-    print (i_list_sticks, end = '')
+    print(i_list_sticks, end='')
+
+# зачёт!
