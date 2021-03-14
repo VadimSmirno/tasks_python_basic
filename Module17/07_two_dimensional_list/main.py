16e9a29b76774c5076b2176e8704a799fd16056b
Insolente13 <lanc05@mail.ru>
2021-03-14 16:57:02 +0300

    Module 17 07 - ok

--- Module17/07_two_dimensional_list/main.py
+++ Module17/07_two_dimensional_list/main.py
@@ -1,4 +1,4 @@
+list_number = [[index for index in range(i, i + 12, 4)] for i in range(1, 4 + 1)]
+print(list_number)
 
-list_number = [[index for index in range(i,i + 12,4) ]  for i in range (1,4+1) ]
-print (list_number)
-
+# зачёт!
