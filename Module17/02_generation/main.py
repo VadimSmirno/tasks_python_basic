b1f3c2623e1179ef9e0777071360d68d58162826
Insolente13 <lanc05@mail.ru>
2021-03-14 20:03:21 +0300

    Module 17 02 - ok

--- Module17/02_generation/main.py
+++ Module17/02_generation/main.py
@@ -1,3 +1,5 @@
 number = int(input('Введите число: '))
-list_number = [1 if index % 2 == 0 else index % 5 for index in range (number)]
-print ('Результат: ', list_number)
+list_number = [1 if index % 2 == 0 else index % 5 for index in range(number)]
+print('Результат: ', list_number)
+
+# зачёт!
