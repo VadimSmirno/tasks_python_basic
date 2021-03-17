34b3893fc96bed22efd8f6daa4393724584af46e
Insolente13 <lanc05@mail.ru>
2021-03-18 00:17:35 +0300

    Module 18 05 - ok

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -2,8 +2,10 @@ password = input('Придумайте пароль: ')
 
 while True:
     if password.isalnum() == True and password.islower() == False and password.isupper() == False and len(password) > 8:
-        print ('Это надежный пароль!')
+        print('Это надежный пароль!')
         break
     else:
-        print ('Пароль ненадёжный. Попробуйте ещё раз.')
+        print('Пароль ненадёжный. Попробуйте ещё раз.')
         password = input('Придумайте пароль: ')
+
+# зачёт!
