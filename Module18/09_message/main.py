424b0bd6b36c75a32d713ac74351093b9bb48edc
likhacheva-s <Kapitoxa93!>
2021-03-17 16:42:24 +0300

    Задача 9

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+text = input('Сообщение: ')
+
+symbol = ''
+list_symbol = []
+for i_text in text:
+    if i_text.isalpha() == True:
+        symbol += i_text
+    if i_text.isalpha() == False:
+        list_symbol.append(symbol)
+        list_symbol.append(i_text)
+        symbol = ''
+
+
+for i_list_symbol in list_symbol:
+    print (i_list_symbol[::-1], end = '')
