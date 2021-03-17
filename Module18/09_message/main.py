7c29db6249d2f2923f5bfcb689d45c97c913ccb3
Insolente13 <lanc05@mail.ru>
2021-03-18 00:21:45 +0300

    Module 18 09 - TODO

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -3,9 +3,9 @@ text = input('Сообщение: ')
 symbol = ''
 list_symbol = []
 for i_text in text:
-    if i_text.isalpha() == True:
+    if i_text.isalpha() == True:  # TODO, лучше if i_text.isalpha()
         symbol += i_text
-    if i_text.isalpha() == False:
+    if i_text.isalpha() == False:  # TODO, лучше else =)
         list_symbol.append(symbol)
         list_symbol.append(i_text)
         symbol = ''
@@ -13,3 +13,7 @@ for i_text in text:
 
 for i_list_symbol in list_symbol:
     print (i_list_symbol[::-1], end = '')
+
+# TODO, пока что решение не оптимально =)
+#  Сообщение: Это задание очень! простое
+#  отЭ еинадаз ьнечо!
