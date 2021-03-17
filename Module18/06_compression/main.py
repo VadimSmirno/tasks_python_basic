aac88aee4f3cbc814f4d6f46f856f29f2dcafc5e
Insolente13 <lanc05@mail.ru>
2021-03-18 00:18:25 +0300

    Module 18 06 - ok

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -2,11 +2,13 @@ line = input('Введите строку: ')
 index = 0
 count = 1
 out = ''
-while index< (len(line) - 1):
-    if line[index] == line[index+1]:
+while index < (len(line) - 1):
+    if line[index] == line[index + 1]:
         count += 1
     else:
         out = out + line[index] + str(count)
         count = 1
-    index +=1
-print(out+ line[index] + str(count))
+    index += 1
+print(out + line[index] + str(count))
+
+# зачёт!
