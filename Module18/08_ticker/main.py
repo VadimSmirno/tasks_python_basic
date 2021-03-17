c78fb55294d9e6c22eff53fb473247dae7408889
Insolente13 <lanc05@mail.ru>
2021-03-18 00:20:10 +0300

    Module 18 08 - ok

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,12 +1,12 @@
-
 first_line = input('Первая строка: ')
 second_line = input('Вторая строка: ')
 
-
-for step in range (1, len(first_line) +1):
+for step in range(1, len(first_line) + 1):
     first_line = first_line[-1:] + first_line[:-1]
     if first_line == second_line:
-        print (f'Первая строка получается из второй со сдвигом {step}')
+        print(f'Первая строка получается из второй со сдвигом {step}')
         break
-    if step == len(first_line ) :
-        print ('Первую строку нельзя получить из второй с помощью циклического сдвига.')
+    if step == len(first_line):
+        print('Первую строку нельзя получить из второй с помощью циклического сдвига.')
+
+# зачёт!
