b7f02d7a123a1315fb6e1b98ea5230c5da04658e
likhacheva-s <Kapitoxa93!>
2021-03-17 12:29:53 +0300

    Задача 7

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+ip_adres = input('Введите IP адрес: ').split('.')
+good_adres = []
+for index in ip_adres:
+    if index.isdigit() == False:
+        print (f'{index} - не целое число')
+        break
+    elif int(index) > 255:
+        print (f'{index} превышает 255')
+        break
+    elif len(ip_adres) != 4:
+        print ('Адрес - это четыре числа, разделённые точками')
+        break
+    else:
+        good_adres.append(index)
+
+if len(good_adres) == 4:
+    print ('IP-адрес корректен')
