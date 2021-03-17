81da8e21a8107b47c0d3111434e3718d17e94e33
Insolente13 <lanc05@mail.ru>
2021-03-18 00:16:21 +0300

    Module 18 03 - ok

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -3,11 +3,13 @@ symbol = '@№$%^&*()'
 
 for i_symbol in symbol:
     if name_file.startswith(i_symbol):
-        print ('Ошибка: название начинается на один из специальных символов')
+        print('Ошибка: название начинается на один из специальных символов')
         break
-    elif name_file.endswith('.txt') or  name_file.endswith('.docx'):
-        print ('Файл назван верно.')
+    elif name_file.endswith('.txt') or name_file.endswith('.docx'):
+        print('Файл назван верно.')
         break
     else:
-        print ('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
+        print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
         break
+
+# зачёт!
