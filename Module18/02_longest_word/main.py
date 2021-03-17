4b95c0c696257f75db639df585f4adf502fb5672
Insolente13 <lanc05@mail.ru>
2021-03-18 00:15:29 +0300

    Module 18 02 - ok

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,9 +1,11 @@
 line = 'Найдите в ней самое длинное слово, выведите  это слово и его длину.'
 list_line = line.split(' ')
-print('Исходная строка: ',line)
+print('Исходная строка: ', line)
 max_length = ''
 for index in list_line:
-    if (len(index)) > len(max_length):
+    if (len(index)) > len(max_length):  # (len(index)) лучше len(index)
         max_length = index
 max_number_symbol = len(max_length)
 print(f'самое длинное слово "{max_length}", оно состоит из {max_number_symbol} букв')
+
+# зачёт!
