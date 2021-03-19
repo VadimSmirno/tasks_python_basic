35a42bd677515e7bba85d36721184aaccf72a420
likhacheva-s <Kapitoxa93!>
2021-03-19 16:45:06 +0300

    Задача 9

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -3,9 +3,9 @@ text = input('Сообщение: ')
 symbol = ''
 list_symbol = []
 for i_text in text:
-    if i_text.isalpha() == True:  # TODO, лучше if i_text.isalpha()
+    if i_text.isalpha():
         symbol += i_text
-    if i_text.isalpha() == False:  # TODO, лучше else =)
+    else:
         list_symbol.append(symbol)
         list_symbol.append(i_text)
         symbol = ''
@@ -14,6 +14,3 @@ for i_text in text:
 for i_list_symbol in list_symbol:
     print (i_list_symbol[::-1], end = '')
 
-# TODO, пока что решение не оптимально =)
-#  Сообщение: Это задание очень! простое
-#  отЭ еинадаз ьнечо!
