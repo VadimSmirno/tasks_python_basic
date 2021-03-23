f6782f6d8519566f747a7765ba5251ac054d60de
likhacheva-s <Kapitoxa93!>
2021-03-23 18:08:39 +0300

    Задача 2

--- Module19/02_geography/main.py
+++ Module19/02_geography/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+country_dict = {}
+
+number_country = int(input('Кол-во стран: '))
+for i_country in range (1, number_country + 1):
+    print (f'{i_country} страна: ', end = '')
+    country = input ('').split()
+    for i_citie in range (1,len(country)):
+        country_dict[country[i_citie]] = country[0]
+print (country_dict)
+
+for num in range(1,4):
+    print (f'{num}. город: ', end = '')
+    citie = input ('')
+    if country_dict.get(citie) == None:
+        print(f'по городу {citie} данных нет\n')
+    else:
+        print (f'Город {citie} расположен в стране {country_dict.get(citie)}\n')
