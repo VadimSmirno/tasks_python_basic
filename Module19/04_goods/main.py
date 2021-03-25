0c6f72e54eedb1aa97abc2f5795be3bcf972934c
Insolente13 <lanc05@mail.ru>
2021-03-26 01:31:46 +0300

    Module 19 04 - TODO

--- Module19/04_goods/main.py
+++ Module19/04_goods/main.py
@@ -1,4 +1,4 @@
-def revision (thing,num):
+def revision(thing, num):
     goods = {
         'Лампа': '12345',
         'Стол': '23456',
@@ -26,14 +26,19 @@ def revision (thing,num):
     }
 
     quantity = store[(goods[thing])][num]['quantity']
-    price =     store[(goods[thing])][num]['price']* store[(goods[thing])][num]['quantity']
+    price = store[(goods[thing])][num]['price'] * store[(goods[thing])][num]['quantity']
     return [quantity, price]
-print ('Лампа -', revision('Лампа', 0)[0], 'шт', 'стоимость', revision('Лампа', 0)[1], 'руб' )
-print('Стол - ',revision('Стол', 0)[0] + revision('Стол', 1)[0], 'шт', 'стоимость', revision('Стол', 0)[1] +
+
+
+print('Лампа -', revision('Лампа', 0)[0], 'шт', 'стоимость', revision('Лампа', 0)[1], 'руб')
+print('Стол - ', revision('Стол', 0)[0] + revision('Стол', 1)[0], 'шт', 'стоимость', revision('Стол', 0)[1] +
       revision('Стол', 1)[1], 'руб')
 print('Диван -', revision('Диван', 0)[0] + revision('Диван', 1)[0], 'шт', 'стоимость', revision('Диван', 0)[1] +
       revision('Диван', 1)[1], 'руб')
-print('Стул -', revision('Стул', 0)[0] + revision('Стул', 1)[0] + revision('Стул', 2)[0], 'шт', 'стоимость', revision('Стул', 0)[1] +
+print('Стул -', revision('Стул', 0)[0] + revision('Стул', 1)[0] + revision('Стул', 2)[0], 'шт', 'стоимость',
+      revision('Стул', 0)[1] +
       revision('Стул', 1)[1] + revision('Стул', 2)[1], 'руб')
 
-
+# TODO, предлагаю упростить решение и решить без функции.
+#  Всего за 2 цикла по словарям. =)
+#  Количество строк должно стать на порядок меньше, как и вычислений значений по ключам словарей.
