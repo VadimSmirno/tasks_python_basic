5176a068af561ab0c0570d40b69986c890bee1c7
Insolente13 <lanc05@mail.ru>
2021-03-26 01:32:25 +0300

    Module 19 05 - ok

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,9 +1,9 @@
-def list_new_hist (new_hist,num):
+def list_new_hist(new_hist, num):
     new_hist = [key for key, values in hist.items() if values == num]
     return new_hist
 
 
-def histogram (string):
+def histogram(string):
     sym_dict = dict()
     for sym in string:
         if sym in sym_dict:
@@ -11,16 +11,17 @@ def histogram (string):
         else:
             sym_dict[sym] = 1
 
-    return  sym_dict
+    return sym_dict
 
-text = input ('Введите текст: ').lower()
+
+text = input('Введите текст: ').lower()
 hist = histogram(text)
-print ('Оригинальный словарь частот:\n')
+print('Оригинальный словарь частот:\n')
 for i_sym in sorted(hist.keys()):
-    print (i_sym, ':', hist[i_sym])
+    print(i_sym, ':', hist[i_sym])
 values = set(hist.values())
-print ('\nИнвертированный словарь частот :\n')
+print('\nИнвертированный словарь частот :\n')
 for i_values in values:
-    print (i_values, ':',list_new_hist (hist,i_values) )
+    print(i_values, ':', list_new_hist(hist, i_values))
 
-
+# зачёт!
