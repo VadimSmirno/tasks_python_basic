1674f79b157ef118b13d29ee2bd27cbf453f472d
likhacheva-s <Kapitoxa93!>
2021-03-25 15:02:51 +0300

    Задача 5

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+def list_new_hist (new_hist,num):
+    new_hist = [key for key, values in hist.items() if values == num]
+    return new_hist
+
+
+def histogram (string):
+    sym_dict = dict()
+    for sym in string:
+        if sym in sym_dict:
+            sym_dict[sym] += 1
+        else:
+            sym_dict[sym] = 1
+
+    return  sym_dict
+
+text = input ('Введите текст: ').lower()
+hist = histogram(text)
+print ('Оригинальный словарь частот:\n')
+for i_sym in sorted(hist.keys()):
+    print (i_sym, ':', hist[i_sym])
+values = set(hist.values())
+print ('\nИнвертированный словарь частот :\n')
+for i_values in values:
+    print (i_values, ':',list_new_hist (hist,i_values) )
+
+
