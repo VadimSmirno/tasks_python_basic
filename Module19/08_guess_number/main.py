6ec130e3e9378ee6b32346ceed892fe608b1a15c
likhacheva-s <Kapitoxa93!>
2021-03-27 19:58:40 +0300

    Задача 8

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+max_number = int(input('Введите максимальное число: '))
+numbers = input('Нужное число есть среди вот этих чисел: ')
+result_set_number = set()
+
+
+while numbers != 'Помогите!' or len(result_set_number) == 1:
+
+
+    set_number = {i_num for i_num in numbers.split() if int(i_num) < max_number}
+    answer = input('Ответ Артема: ').lower()
+    print(set_number)
+    if answer == 'да':
+        result_set_number.update(set_number)
+        print(result_set_number)
+    elif answer ==  'нет':
+        result_set_number -= set_number
+        print(result_set_number)
+    set_number = set()
+    numbers = input('Нужное число есть среди вот этих чисел: ')
+
+
+print ('Артём мог загадать следующие числа: ', end='')
+for i_result in result_set_number:
+    print (i_result, end='')
+
