ffd9aae13dfff2092f916b251bc16f3097dcd890
likhacheva-s <Kapitoxa93!>
2021-03-28 13:49:42 +0300

    Задача 8

--- Module19/08_guess_number/main.py
+++ Module19/08_guess_number/main.py
@@ -1,28 +1,24 @@
 max_number = int(input('Введите максимальное число: '))
-numbers = input('Нужное число есть среди вот этих чисел: ')
-result_set_number = set()
-
-# TODO, предлагаю создать множество "допустимых чисел" от 1 до числа пользователя включительно.
-#  И сокращать/изменять его исходя из ответа пользователя.
-#  Если в мжнестве "допустимых чисел" осталось только 1 число, из цикла выходим.
-
-while numbers != 'Помогите!' or len(result_set_number) == 1:
+numbers = ''
+result_set_number = {num for num in range(1, max_number+1)}
 
 
-    set_number = {i_num for i_num in numbers.split() if int(i_num) <= max_number}
-    answer = input('Ответ Артема: ').lower()
-    print(set_number)
-    if answer == 'да':
-        result_set_number.update(set_number)
-        print(result_set_number)
-    elif answer ==  'нет':
-        result_set_number -= set_number
-        print(result_set_number)
-    set_number = set()
+while numbers != 'Помогите!':
     numbers = input('Нужное число есть среди вот этих чисел: ')
+    set_numbers = {int(i_num) for i_num in numbers.split()}
+    answer = input('Ответ Артема: ').lower()
+    if answer == 'да':
+        result_set_number = set_numbers.intersection(result_set_number)
+
+    elif answer ==  'нет':
+        result_set_number -= set_numbers
+
+    if len(result_set_number) == 1:
+        break
 
 
-print ('Артём мог загадать следующие числа: ', end='')
+
+print ('Артем мог загадать следующие числа: ', end='')
 for i_result in result_set_number:
-    print (i_result, end='')
+    print (i_result, end=' ')
 
