e65b8f1524fe194264d643bbfd9b807c8d0db653
Insolente13 <lanc05@mail.ru>
2021-03-28 23:28:10 +0300

    Module 19 10 - ok

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,18 +1,20 @@
-def palindrome (string):
-   elem_dict = {}
-   for symbol in string:
-       elem_dict[symbol] = elem_dict.get (symbol, 0) + 1
+def palindrome(string):
+    elem_dict = {}
+    for symbol in string:
+        elem_dict[symbol] = elem_dict.get(symbol, 0) + 1
+
+    count = 0
+    for i_value in elem_dict.values():
+        if i_value % 2 != 0:
+            count += 1
+
+    return count <= 1
 
 
-   count = 0
-   for i_value in elem_dict.values():
-       if i_value % 2 != 0:
-           count += 1
-
-   return  count <= 1
-
-string  = input('Введите строку: ')
-if palindrome (string):
-    print ('Можно сделать палиндром')
+string = input('Введите строку: ')
+if palindrome(string):
+    print('Можно сделать палиндром')
 else:
-    print ('Нельзя сделать палиндром')
+    print('Нельзя сделать палиндром')
+
+# зачёт!
