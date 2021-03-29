350ba4331638bdaed3da2754575afb384b560111
likhacheva-s <Kapitoxa93!>
2021-03-29 20:55:49 +0300

    Module 20

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def is_prime (number):
+    count = 0
+    for i_num in range (1, number + 1):
+        if number % i_num == 0:
+            count += 1
+    if count == 2:
+        return True
+    else:
+        return False
+
+
+def list_object (obj_ect):
+    return [i_valuse for i_key, i_valuse in enumerate(obj_ect) if is_prime(i_key)==True]
