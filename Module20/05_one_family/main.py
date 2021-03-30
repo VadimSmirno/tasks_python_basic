056dc70a2dc84edc9115f66850ece85650ede714
likhacheva-s <Kapitoxa93!>
2021-03-30 18:04:20 +0300

    Module 20

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+family_dict = {
+    ('Петров', 'Иван') : 25,
+    ('Петрова','Анна') : 24,
+    ('Петров', 'Николай'): 62,
+
+    ('Сидоров', 'Никита') : 35,
+    ('Сидорова', 'Алина') : 34,
+    ('Сидоров', 'Павел') : 10
+
+}
+
+
+surname = input('Введите фамилию семьи: ')
+for i_person,age in family_dict.items():
+    if i_person[0] == surname or i_person[0] == surname + 'а':
+        print (i_person[0],i_person[1],age)
--- Module20/06_pairs/main.py
+++ Module20/06_pairs/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+list_number = [num for num in range(10)]
+print (list_number)
+new_list_number = [(num,num +1) for num in range(0,10,2)] # работает не совсеми списками
+new_list_number2 = [(value,list_number[index+1])for index, value in enumerate(list_number) if index % 2 == 0]
+print (new_list_number)
+print (new_list_number2)
+
