5fa23eaf2bdf845b363f84236528ceca20c6ce76
likhacheva-s <Kapitoxa93!>
2021-04-03 16:59:49 +0300

    Module 20

--- Module20/08_contacts_3/main.py
+++ Module20/08_contacts_3/main.py
@@ -1,12 +1,13 @@
 d_phonebook = {}
 finish_dict = {}
+flag = False
 while True:
     act = input('Добавить контакт или найти? ').lower()
     if act == 'добавить':
         name = input('\nИмя? ').lower()
         surname = input('Фамилия? ').lower()
         telephone = input('Номер телефона: ')
-        if (name,surname) in d_phonebook:
+        if (name,surname) in finish_dict:
             print ('\nЭтот человек уже есть в списке Ваших контактов.')
         d_phonebook = {
             (name,surname): telephone
@@ -14,9 +15,10 @@ while True:
         finish_dict.update(d_phonebook)
         print('\n',finish_dict,'\n')
     if act == 'найти':
-        look_surname = input('Человека с какой фамилией Вы хотите найти? ').lower()
+        look_surname = input('\nЧеловека с какой фамилией Вы хотите найти? ').lower()
         for i_person, phone in finish_dict.items():
             if look_surname in i_person:
                 print(i_person[0], i_person[1],'-', phone)
-
-
+                flag = True
+        if flag == False:
+            print ('\nЧеловека с такой фамилией нет в Вашем списке')
