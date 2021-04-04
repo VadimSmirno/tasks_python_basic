fe2ec9bc29137d84fa201fff9508780a60b8f1de
likhacheva-s <Kapitoxa93!>
2021-04-04 12:43:25 +0300

    Module 20

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def min_len (string, cortege):
+    return min(len(string),len(cortege))
+
+string = 'abcd'
+cortege = (10,20,30,40,52)
+
+result = ((string[i_sym],cortege[i_sym])for i_sym in range(min_len(string,cortege)))
+print (result)
+for element in result:
+    print (element)
