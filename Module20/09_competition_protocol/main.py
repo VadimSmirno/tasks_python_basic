6ecef273683c0324eddbd5694c802d7f3364703e
likhacheva-s <Kapitoxa93!>
2021-04-04 12:25:09 +0300

    Module 20

--- Module20/09_competition_protocol/main.py
+++ Module20/09_competition_protocol/main.py
@@ -1,17 +1,20 @@
-# score_table = {}
-# number = int(input('Сколько записей вносится в протокол?'))
-# for time in range(1,number +1):
-#     print (time, 'запись:', end='')
-#     ball, name = input().split()
-#     ball = int(ball)
-#     if name in score_table:
-#         if ball > score_table[name][0]:
-#             score_table[name][0] = ball
-#             score_table[name][1] = time
-#     else:
-#         score_table[name] = [ball, time]
-# scores = list(score_table.items())
-#
-# print (scores)
-a = [('Jack', [95715, 6]), ('qwerty', [197128, 5]), ('Alex', [95715, 3]), ('M', [95715, 9])]
+score_table = {}
+number = int(input('Сколько записей вносится в протокол?'))
+for time in range(1,number +1):
+    print (time, 'запись:', end='')
+    ball, name = input().split()
+    ball = int(ball)
+    if name in score_table:
+        if ball > score_table[name][0]:
+            score_table[name][0] = ball
+            score_table[name][1] = time
+    else:
+        score_table[name] = [ball, time]
+scores = list(score_table.items())
 
+def score_key(key):
+    return key[1][0]- key[1][1]
+
+scores.sort(key=score_key,reverse=True)
+for num in range(3):
+    print (num+1, 'место', scores[num][0], scores[num][1][0])
