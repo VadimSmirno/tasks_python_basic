fd464a59821a3a8795788417962cde6ab499177b
likhacheva-s <Kapitoxa93!>
2021-04-17 09:50:19 +0300

    Модуль 22

--- Module22/05_save/main.py
+++ Module22/05_save/main.py
@@ -2,23 +2,19 @@ import  os
 def file_write():
     with open(file_name, 'w') as file_object:
         file_object.write(text)
-
+text = input('Введите строку: ')
 
 while True:
-    # TODO, строку для ввода и имя файла предлагаю вводить вне текущего цикла =)
-    text = input('Введите строку: ')
+
     print('Куда хотите сохранить документ? Введите последовательность папок (через пробел):')
-    way = input('')
-    # TODO, переменная way содержит список папок, из которых необходимо создать путь к директории с файлом и проверить его
-    #  наличие. Давайте попробуем произвести действия именно с переменной way.
+    way = '\\'.join(input('').split())
     file_name = input((f'Введите имя файла: \n'))
-    abs_path = os.path.abspath(file_name)
-    # print(abs_path)
+    abs_path = os.path.abspath(os.path.join(os.path.sep,way))
+    print(abs_path)
     check_file = os.path.exists(abs_path)
     print(check_file)
     if check_file:
         break
-
 
 if check_file:
     ans_q = input(f'Вы действительно хотите перезаписать файл? \n')
