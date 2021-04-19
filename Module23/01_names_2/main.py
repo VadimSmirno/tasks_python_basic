b5823f15514c2c3a1b230f83a8d9a0e95a27918f
likhacheva-s <Kapitoxa93!>
2021-04-19 16:26:19 +0300

    Module23

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+def errors (error):
+    with open('errors.log', 'a') as file_errors:
+        file_errors.write(f'{str(error)}\n')
+
+
+summ_symbol = 0
+count = 0
+try:
+    with open('people.txt','r',encoding='UTF-8') as file_people:
+        for i_sym in file_people:
+            count += 1
+            line = len(i_sym)
+            if i_sym.endswith('\n'):
+                line -= 1
+            if line < 3:
+                try:
+                    raise BaseException
+                except BaseException:
+                    print(f'Длина {count} строки меньше 3 символов')
+                    errors(BaseException)
+            summ_symbol += line
+
+except FileNotFoundError:
+    print ('Файл не найден')
+    errors(FileNotFoundError)
+
+finally:
+    print('Сумма символов', summ_symbol)
