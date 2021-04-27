5d5524d466fdbf4c3c6d4722fde4dd7950c1f809
likhacheva-s <Kapitoxa93!>
2021-04-27 16:07:05 +0300

    Module 24

--- Module24/04_fathers_mothers_kids/main.py
+++ Module24/04_fathers_mothers_kids/main.py
@@ -1,1 +1,45 @@
-# TODO здесь писать код
+class Parents:
+
+    def __init__(self,name,ages,list_children):
+        self.name = name
+        self.ages = ages
+        self.list_children = list_children
+
+    def print_info(self):
+        print (f'Мое имя {self.name}, мне {self.ages} лет')
+        print('Мои дети: ',end='')
+        for i_children in self.list_children:
+            print(i_children, end=' ')
+
+    def calm_the_child(self):
+        print('Ребенок успокоился')
+
+    def feed_the_baby(self):
+        print ('Ребенок покормлен')
+
+
+class Children:
+
+    def __init__(self,name,ages,calmness,hunger):
+        self.name = name
+        self.ages = ages
+        self.calmness = calmness
+        self.hunger = hunger
+
+
+    def calmness(self):
+        if self.calmness == 'Нет':
+            perens.calm_the_child()
+
+
+
+    def  hunger(self):
+        if self.hunger == 'Нет':
+            perens.feed_the_baby()
+
+
+perens = Parents('Коля', 31, ['Ваня','Лиза'])
+perens2 = Parents('Маша', 27, ['Ваня','Лиза'])
+children1 = Children('Ваня', 1, 'Да','Нет')
+children2 = Children('Лиза', 2, 'Нет','Нет')
+children2.hunger()
