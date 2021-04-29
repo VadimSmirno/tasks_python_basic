7f288bad23e74d6189a3f023333f2d7c561f1c86
likhacheva-s <Kapitoxa93!>
2021-04-29 14:42:03 +0300

    Module 24

--- Module24/06_magic/main.py
+++ Module24/06_magic/main.py
@@ -1,20 +1,67 @@
 class Water:
 
-    name = 'Вода'
+    """"" Класс вода """""
+
+    def __str__(self):
+        return 'Вада'
 
     def __add__(self, other):
-        return Air()
-
+        if isinstance(other,Air):
+            return Storm()
+        elif isinstance(other,Fire):
+            return Steam() # пар
+        elif isinstance(other,Earth):
+            return Mud()  # грязь
 
 class Air:
 
-    name = 'Воздух'
+    """" Класс воздух """""
 
+    def __str__(self):
+        return 'Воздух'
+
+    def __add__(self, other):
+        if isinstance(other,Fire):
+            return Lightning()  #молния
+        elif isinstance(other,Earth):
+            return Dust() # пыль
 class Fire:
 
-    name = 'Огонь'
+    """" Класс огонь """""
 
+    def __str__(self):
+        return 'Огонь'
 
+    def __add__(self, other):
+        if isinstance(other,Earth):
+            return Lava()
 class Earth:
+    """" Класс земля """""
 
-    name = 'Земля'
+    def __str__(self):
+        return 'Земля'
+class Storm:
+    def answer(self):
+        return 'Вода + Воздух'
+
+class Steam:
+    def answer(self):
+        return 'Вода + Огонь'
+
+
+class Mud:
+    def answer(self):
+        return 'Вода + Земля'
+class Lightning:
+    def answer(self):
+        return 'Воздух + Огонь'
+
+class Dust:
+    def answer(self):
+        return 'Воздух + Земля'
+
+class Lava:
+    def ansver(self):
+        return  'Огонь + Земля'
+
+
