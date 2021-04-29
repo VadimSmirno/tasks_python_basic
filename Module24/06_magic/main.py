c6cc6644c502782ee2fa5036327bdca62493c5d9
likhacheva-s <Kapitoxa93!>
2021-04-29 10:07:07 +0300

    Module 24

--- Module24/06_magic/main.py
+++ Module24/06_magic/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+class Water:
+
+    name = 'Вода'
+
+    def __add__(self, other):
+        return Air()
+
+
+class Air:
+
+    name = 'Воздух'
+
+class Fire:
+
+    name = 'Огонь'
+
+
+class Earth:
+
+    name = 'Земля'
