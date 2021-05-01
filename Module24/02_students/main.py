9109bfaa0d31fb774baefddc7465eeda4266e030
Insolente13 <lanc05@mail.ru>
2021-05-01 21:19:40 +0300

    Module 24 01, 02, 04 - ok, 03, 05, 06 - TODO, another - not yet

--- Module24/02_students/main.py
+++ Module24/02_students/main.py
@@ -1,6 +1,5 @@
 import random
 
-students = []
 
 class Student:
 
@@ -13,15 +12,18 @@ class Student:
         print(f'{self.name} из группы № {self.number_grupp}: оценки за 5 предметов: {self.school_performance}')
 
 
+students = []
 
 people = ['Дмитрий', 'Максим', 'Сергей', 'Андрей', 'Алексей', 'Артём', 'Илья', 'Кирилл', 'Михаил', 'Никита']
 
 for i_name in people:
     name, number_grupp, school_performance = i_name, random.randint(1, 10), [random.randint(1, 5) for _ in range(5)]
-    student = Student(name, number_grupp, sum(school_performance)/5)
+    student = Student(name, number_grupp, sum(school_performance) / 5)
     students.append(student)
-
 
 sort_lst = sorted(students, key=lambda k: k.school_performance)
 for i_info in sort_lst:
-    print(f'Студент {i_info.name} учится в группе № {i_info.number_grupp}, средний бал по успеваемости: {i_info.school_performance}')
+    print(
+        f'Студент {i_info.name} учится в группе № {i_info.number_grupp}, средний бал по успеваемости: {i_info.school_performance}')
+
+# зачёт!
--- Module24/03_circle/main.py
+++ Module24/03_circle/main.py
@@ -18,11 +18,17 @@ class Circle:
     def big_circle(self, coefficient):
         return math.pi * self.radius ** 2 * coefficient
 
-    # TODO, второй круг передавать в метод не нужно.
+    # , второй круг передавать в метод не нужно.
     #  Предлагаю сравнивать со "своими" аргументами =)
     #  через self.
     def crossing(self, circle):
         if isinstance(circle, Circle):
+            # TODO, верно! но вместо circle2.x необходимо обращаться к своим координатам (self.x и т.д.)
+            #  Мы создали класс Круг.
+            #  Далее мы создали 2 объекта класса, которые являются Окружностями, т.к. были созданы от класса Круг.
+            #  У каждого объекта класса есть свои методы и аргументы.
+            #  Про circle2 класс не в курсе, НО, класс знает про circle, т.к. мы передаём его в метод класса и знает
+            #  про свои координаты =)
             d = math.sqrt((circle.x - circle2.x) ** 2 + (circle.y - circle2.y) ** 2)
             if d > (circle.radius + circle2.radius) or 0 < d < math.fabs(circle.radius - circle2.radius):
                 print('общих точек нет')
@@ -39,3 +45,6 @@ circle2 = Circle(2, 4, 3)
 print(circle1.perimeter())
 print(circle1.big_circle(coefficient))
 circle1.crossing(circle1)
+# TODO, таким образом мы проверяем пересекается ли круг с самим собой.
+#  Необходимо передавать circle2
+
