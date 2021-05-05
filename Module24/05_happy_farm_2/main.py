d59e8d93119e046d93183b697e44081cb8b37a12
Insolente13 <lanc05@mail.ru>
2021-05-05 23:47:57 +0300

    Module 24 01, 03, 02, 04, 06, 05, 09 - ok, 07 - TODO

--- Module24/05_happy_farm_2/main.py
+++ Module24/05_happy_farm_2/main.py
@@ -2,15 +2,22 @@ from garden import PotetoGarden
 from garden import Garden
 from garden import Potesto
 
-
+# TODO, должна быть только 1 грядка за которой ухаживает садовник. my_garden - лишняя =)
 my_garden = PotetoGarden(5)
 my_garden.are_all_ripe()
+
+# TODO, сейчас назвали садовника "мой сад"? =) Возможно, стоит придумать ему другое название.
 my_garden2 = Garden('Остин', 5)
 my_garden2.look_after()
+print(my_garden2.ridge.potetoes)
 for _ in range(3):
     my_garden.grow_al()
     my_garden.are_all_ripe()
     my_garden2.look_after()
+print(my_garden2.ridge.potetoes)
+
+# зачёт!
+
 
 
 
