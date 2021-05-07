e8a65758360b2bab6c2256613ac5e8f90741c1b5
likhacheva-s <Kapitoxa93!>
2021-05-07 20:19:36 +0300

    Module25

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+class MyDict:
+    def __init__(self,key,value):
+        self.__key = key
+        self.__value = value
+
+    def __str__(self):
+        return f'{self.__key} : {self.__value}'
+
+
+    def get_key(self):
+        return self.__key
+
+    def get_value(self):
+        return self.__value
+
+    def set_value(self,value):
+        return self.__value==value
