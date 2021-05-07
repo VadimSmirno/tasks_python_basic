95835a3d9098c339c9c0e6f52e2949c8e3ce1a30
likhacheva-s <Kapitoxa93!>
2021-05-07 19:55:40 +0300

    Module25

--- Module25/01_taxes/main.py
+++ Module25/01_taxes/main.py
@@ -1,1 +1,79 @@
-# TODO здесь писать код
+class Property:
+
+    def __init__(self,worth):
+        self.worth = worth
+
+
+
+    def tax_calculationApartment(self):
+        result = self.worth/1000
+        self.print_info(result)
+        return result
+
+    def tax_calculationCar(self):
+        result = self.worth / 200
+        self.print_info(result)
+        return result
+
+    def tax_calculationCountryHouse(self):
+        result = self.worth/500
+        self.print_info(result)
+        return result
+
+    def print_info(self,result):
+        print (f'налог составил: {result}')
+
+
+class  Apartment(Property):
+    def __init__(self,worth):
+        super().__init__(worth)
+
+class  Car(Property):
+    def __init__(self,worth):
+        super().__init__(worth)
+
+class  CountryHouse(Property):
+    def __init__(self,worth):
+        super().__init__(worth)
+
+
+def check(res, money):
+    if res < money:
+        money -= result
+        print('Налог оплачен, осталось {}'.format(money))
+        return True
+    else:
+        print('У Вас недостаточно средств')
+        return False
+
+money = int(input('Какими средствами располагаете? '))
+for _ in range(3):
+    property = input('За какое имущество хотите заплатить налог? ').lower()
+    price = int(input(f'Какова стоимость {property}? '))
+    if property == 'квартира':
+        apartment = Apartment(worth=price)
+        result = apartment.tax_calculationApartment()
+        if not check(result,money):
+            break
+        money-=result
+    elif property == 'машина':
+        car = Car(worth=price)
+        car.tax_calculationCar()
+        result_car = car.tax_calculationCar()
+        if not check(result_car, money):
+            break
+        money -= result_car
+    elif property == 'дача':
+        country_house = CountryHouse(worth=price)
+        country_house.tax_calculationCountryHouse()
+        result_country_house = country_house.tax_calculationCountryHouse()
+        if not check(result_country_house,money):
+            break
+        money -= result_country_house
+else:
+    print (f'Налог уплачен осталось средств {money}')
+
+
+
+
+
--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -1,1 +1,48 @@
-# TODO здесь писать код
+import random
+
+
+class KillError(Exception):
+    pass
+
+class DrunkError(Exception):
+    pass
+
+class CarCrashError(Exception):
+    pass
+
+class GluttonyError(Exception):
+    pass
+
+class DepressionError(Exception):
+    pass
+
+
+
+def one_day():
+    return (random.randint(1, 7),random.randint(1, 50))
+
+number_karma = 0
+while number_karma < 500:
+    number_karma += one_day()[0]
+    number_exception = one_day()[1]
+
+    try:
+        if number_exception == 1:
+            print ('Карма нарушена')
+            raise KillError ('Убил комара')
+        elif number_exception == 10:
+            raise DrunkError ('Выпил')
+        elif number_exception == 20:
+            raise CarCrashError('Пьяный за рулем')
+        elif number_exception == 30:
+            raise GluttonyError('Возжелал того, чего желать не стоит')
+        elif number_exception == 40:
+            raise DepressionError ('Впал в депрессию')
+
+    except (KillError,DrunkError,CarCrashError,GluttonyError,DepressionError) as err:
+        with open('karma.log', 'a') as fail:
+            fail.write(f'карма нарушена {err}\n')
+    print(f'Уровень кармы {number_karma}')
+
+
+
