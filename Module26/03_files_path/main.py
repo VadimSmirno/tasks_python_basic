10317cb6184d46d2800cbc43ff0c963fb1a27b2f
likhacheva-s <Kapitoxa93!>
2021-05-14 13:26:13 +0300

    Module25

--- Module26/03_files_path/main.py
+++ Module26/03_files_path/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import os
+
+directory = input('Введите директорию ')
+
+def gen_files_path(directory:str):
+    result = os.walk(directory)
+    yield result
+
+
+function_result = gen_files_path(directory)
+for i_generator in function_result:
+    for j_directory in i_generator:
+        print(j_directory)
--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+class Hofstadter:
+    def __init__(self, lst):
+        self.lst = lst[:]
+
+    def __next__(self):
+        try:
+            q = self.lst[-self.lst[-1]] + self.lst[-self.lst[-2]]
+            self.lst.append(q)
+            return q
+        except IndexError:
+            raise StopIteration()
+
+    def __iter__(self):
+        return self
+
+
+
+hofstadter = Hofstadter([1,1])
+print ([next(hofstadter) for  _ in range(10)])
