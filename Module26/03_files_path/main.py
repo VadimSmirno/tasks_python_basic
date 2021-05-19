3181de255476ea54647f7b9246677e951c5a6dc7
likhacheva-s <Kapitoxa93!>
2021-05-19 16:14:00 +0300

    Module 26

--- Module26/03_files_path/main.py
+++ Module26/03_files_path/main.py
@@ -5,16 +5,18 @@ name_faile = input('Какой файл ищем? ')
 
 
 def gen_files_path(directory: str, name_faile: str):
+    count = 0
     for i_name_faile in os.listdir(directory):
         if os.path.isfile(i_name_faile):
             yield i_name_faile
         elif os.path.isdir(i_name_faile):
             if os.path.basename(i_name_faile) == name_faile:
                 return os.path.join(directory, i_name_faile)
-        else:  # TODO, блок else получился лишним.
-               #  Вместо этого, стоит реализовать цикл по запуску функции ниже с yield =)
-            gen_files_path(os.path.join(directory, i_name_faile), name_faile)
 
+        while count < len(os.listdir(directory)):
+            count +=1
+            gen_files_path(os.path.join(directory, i_name_faile), name_faile)
+            yield os.path.join(directory, i_name_faile)
 
 function_result = gen_files_path(directory, name_faile)
 for i in function_result:
@@ -22,3 +24,4 @@ for i in function_result:
 
 # D:\python_basic
 # 03_files_path
+
--- Module26/05_str_count/main.py
+++ Module26/05_str_count/main.py
@@ -3,25 +3,18 @@ import os
 directory = input('Введите директорию ')
 
 
-def gen_files_path(directory):
+def gen_files_path(directory:str):
     count_line = 0
 
-    # TODO, в текущем задании рекурсия не нужна. Получаем на вход директории и идём по её файлам =)
-    #  Для сокращения количество действий в коде, предлагаю первым делом внутри цикла создать переменную, равную
-    #  os.path.join(directory, i_name_faile) и далее в коде работать с ней =)
-
     for i_name_faile in os.listdir(directory):
-        if os.path.isdir(os.path.join(directory, i_name_faile)):
-            gen_files_path(os.path.join(directory, i_name_faile))
-        elif os.path.isfile(os.path.join(directory, i_name_faile)) and i_name_faile.endswith('.py'):
-            print(os.path.join(directory, i_name_faile))
-            with open(os.path.join(directory, i_name_faile), 'r') as fail:
+        path = os.path.join(directory, i_name_faile)
+        if os.path.isfile(path) and i_name_faile.endswith('.py'):
+            print(path)
+            with open(path, 'r') as fail:
                 for i_line in fail.readlines():
                     if '#' not in i_line and i_line:
                         count_line += 1
             yield count_line
-        else:
-            gen_files_path(os.path.join(directory, i_name_faile))
 
 
 function_result = gen_files_path(directory)
