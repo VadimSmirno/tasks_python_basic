c72c081fc26a832fdb5f21858489b5129dba6936
Insolente13 <lanc05@mail.ru>
2021-05-20 07:53:48 +0300

    Module 26 - ok

--- Module26/06_linked_list/main.py
+++ Module26/06_linked_list/main.py
@@ -1,10 +1,11 @@
 class Node:
-    def __init__(self, value = None, next = None):
+    def __init__(self, value=None, next=None):
         self.value = value
         self.next = next
 
     def __str__(self):
         return f'Node [{str(self.value)}]'
+
 
 class LinkedList:
     def __init__(self):
@@ -12,7 +13,7 @@ class LinkedList:
         self.length = 0
 
     def __str__(self):
-        if self.head is not  None:
+        if self.head is not None:
             current = self.head
             values = [str(current.value)]
             while current.next is not None:
@@ -21,7 +22,7 @@ class LinkedList:
             return '[{values}]'.format(values=' '.join(values))
         return 'LinkedList[]'
 
-    def append(self,elem):
+    def append(self, elem):
         new_node = Node(elem)
         if self.head is None:
             self.head = new_node
@@ -33,11 +34,11 @@ class LinkedList:
         last.next = new_node
         self.length += 1
 
-    def remove(self,index):
+    def remove(self, index):
         cur_node = self.head
         cur_index = 0
         if self.length == 0 or self.length <= index:
-            raise  IndexError
+            raise IndexError
 
         if cur_node is not None:
             if index == 0:
@@ -61,20 +62,19 @@ class LinkedList:
         curr_index = 0
         while curr_index <= index:
             if curr_index == index:
+                # TODO, пока что получаем ошибку в этом месте кода при запуске кода =)
+                #  AttributeError: 'NoneType' object has no attribute 'value'
                 return node.value
             curr_index += 1
             node = node.next
-
-
-
 
 
 my_list = LinkedList()
 my_list.append(10)
 my_list.append(20)
 my_list.append(30)
-print (my_list)
-print ()
+print(my_list)
+print()
 # my_list.remove(0)
-print (my_list.get(3))
+print(my_list.get(3))
 print(my_list)
