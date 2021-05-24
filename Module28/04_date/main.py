9d5827a49cef30b39343f79c83f0d48d97cece37
likhacheva-s <Kapitoxa93!>
2021-05-24 18:34:08 +0300

    Module 28

--- Module28/04_date/main.py
+++ Module28/04_date/main.py
@@ -1,1 +1,28 @@
+class Date:
 
+    def __init__(self):
+        self.date =None
+
+    @classmethod
+    def from_string(cls,date):
+        lst_date = date.split('-')
+        return  f'День: {lst_date[0]}\t' \
+                f'Месяц: {lst_date[1]}\t' \
+                f'Год: {lst_date[2]}\n'
+
+    @classmethod
+    def  is_date_valid(cls,date):
+        lst_date = date.split('-')
+        number = int(lst_date[0])
+        month = int(lst_date[1])
+        year = int(lst_date[2])
+        if 0 < number <= 31 and 0 < month <= 12 and year > 0:
+            return True
+        else:
+            return False
+
+
+date = Date.from_string('10-12-2077')
+print(date)
+print(Date.is_date_valid('10-12-2077'))
+print(Date.is_date_valid('40-12-2077'))
