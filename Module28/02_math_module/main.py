e2d32cf5b52f796667b08a12378ad1eaf1a372fc
likhacheva-s <Kapitoxa93!>
2021-05-24 18:36:17 +0300

    Module 28

--- Module28/02_math_module/main.py
+++ Module28/02_math_module/main.py
@@ -6,21 +6,24 @@ class MyMath:
         self.radius = None
         self.cube_side_length = None
 
+    @classmethod
     def circumference(self,radius):
         self.radius = radius
         return round(2 * radius * math.pi, 3)
-
+    @classmethod
     def area_circle(self,radius):
         return round(math.pi * radius**2,3 )
 
+    @classmethod
     def cube_volume(self,cube_side_length):
         return cube_side_length**3
 
+    @classmethod
     def sphere_area(self,radius):
         return round(4 * math.pi * radius ** 2)
 
-res_1 = MyMath()
-print(res_1.circumference(5))
-res_2 = MyMath()
-print (res_2.area_circle(6))
+res_1 = MyMath.circumference(5)
+print(res_1)
+res_2 = MyMath.area_circle(6)
+print (res_2)
 
