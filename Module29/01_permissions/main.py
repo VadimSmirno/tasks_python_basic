8d1ecfe4ccede2a2e324e27bfaefdf0734585a2f
likhacheva-s <Kapitoxa93!>
2021-05-29 18:18:24 +0300

    Module 29

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+def check_permission(user_name:str)->Callable:
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapped_func(*args,**kwargs):
+            try:
+                if user_name != 'admin'.lower():
+                    raise PermissionError (f'У пользователя недостаточно прав, чтобы выполнить функцию {func.__name__}')
+                result = func(*args,**kwargs)
+                return result
+            except PermissionError as err:
+                print('PermissionError:', err)
+        return wrapped_func
+    return decorator
+
+
+user_permissions = ['admin']
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+
+@callback('//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+
+route = app.get('//')
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
