e39be12932716e9f03990cbf161ef6f3e25b623d
Insolente13 <lanc05@mail.ru>
2021-05-29 20:14:25 +0300

    Module 29 01 - TODO, another - not yet

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,24 +1,33 @@
 from typing import Callable
 import functools
-def check_permission(user_name:str)->Callable:
+
+
+def check_permission(user_name: str) -> Callable:
     def decorator(func):
         @functools.wraps(func)
-        def wrapped_func(*args,**kwargs):
+        def wrapped_func(*args, **kwargs):
             try:
+                # TODO, стоит проверять наличие пользователя в списке user_permissions =)
                 if user_name != 'admin'.lower():
-                    raise PermissionError (f'У пользователя недостаточно прав, чтобы выполнить функцию {func.__name__}')
-                result = func(*args,**kwargs)
+                    raise PermissionError(f'У пользователя недостаточно прав, чтобы выполнить функцию {func.__name__}')
+                result = func(*args, **kwargs)
                 return result
             except PermissionError as err:
                 print('PermissionError:', err)
+
         return wrapped_func
+
     return decorator
 
 
 user_permissions = ['admin']
+
+
 @check_permission('admin')
 def delete_site():
     print('Удаляем сайт')
+
+
 @check_permission('user_1')
 def add_comment():
     print('Добавляем комментарий')
