95646e8df3f5fbb5ef576afed20ab305b52f4da5
likhacheva-s <Kapitoxa93!>
2021-06-10 20:28:15 +0300

    Module 30

--- Module30/04_code_readability/main.py
+++ Module30/04_code_readability/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+
+result = list(filter(lambda x: all(map(lambda i: x % i != 0, range(2, int(x ** 0.5) + 1))), range(2, 10000)))
+print (result)
+
+lst = []
+for num in range(2,1001):
+    prime = True
+    for i in range(2,num):
+        if (num % i==0):
+            prime = False
+    if prime:
+       lst.append(num)
+
+print (lst)
--- Module30/05_palindrome_the_return/main.py
+++ Module30/05_palindrome_the_return/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import collections
+
+def can_be_poly(str):
+    pass
+
+
+print(can_be_poly('ababc'))
+
+print(can_be_poly('abbbc'))
