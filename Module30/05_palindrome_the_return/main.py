e327dade3f6355f73cd157075ef7163bd8c680e7
likhacheva-s <Kapitoxa93!>
2021-06-11 11:32:08 +0300

    Module 30

--- Module30/05_palindrome_the_return/main.py
+++ Module30/05_palindrome_the_return/main.py
@@ -1,9 +1,18 @@
-import collections
+from collections import defaultdict
 
 def can_be_poly(str):
-    pass
-
+    str_lower = str.lower()
+    dct_count = defaultdict(int)
+    for symbol in str_lower:
+        if ord(symbol) >= 97 and ord(symbol) <=122:
+            dct_count[symbol] += 1
+    middle = ''
+    for symbol in dct_count:
+        if middle and dct_count[symbol] % 2 == 1:
+            return False
+        elif dct_count[symbol] % 2 == 1:
+            middle = symbol
+    return  True
 
 print(can_be_poly('ababc'))
-
 print(can_be_poly('abbbc'))
