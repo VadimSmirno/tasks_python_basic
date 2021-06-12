bbbcba9fbcab6e279b5834a43720d7cff992a2a5
likhacheva-s <Kapitoxa93!>
2021-06-12 19:18:25 +0300

    Module 30

--- Module30/05_palindrome_the_return/main.py
+++ Module30/05_palindrome_the_return/main.py
@@ -1,21 +1,27 @@
-from collections import defaultdict
+import collections
+
+# def can_be_poly(str):
+#     str_lower = str.lower()
+#     dct_count = defaultdict(int)
+#     for symbol in str_lower:
+#         if ord(symbol) >= 97 and ord(symbol) <=122:
+#             dct_count[symbol] += 1
+#     middle = ''
+#     for symbol in dct_count:
+#         if middle and dct_count[symbol] % 2 == 1:
+#             return False
+#         elif dct_count[symbol] % 2 == 1:
+#             middle = symbol
+#     return  True
 
 def can_be_poly(str):
-    str_lower = str.lower()
-    dct_count = defaultdict(int)
-    for symbol in str_lower:
-        if ord(symbol) >= 97 and ord(symbol) <=122:
-            dct_count[symbol] += 1
-    middle = ''
-    for symbol in dct_count:
-        if middle and dct_count[symbol] % 2 == 1:
-            return False
-        elif dct_count[symbol] % 2 == 1:
-            middle = symbol
-    return  True
-
+    result = filter(lambda x : x % 2 != 0,collections.Counter(str).values())
+    if len(list(result))> 1:
+        return False
+    else:
+        return True
+#
 print(can_be_poly('ababc'))
 print(can_be_poly('abbbc'))
 
-# TODO, перелагаю попробовать решить задание в одну сроку =)
-#  Стоит использовать функции filter + lambda. Возможно, упростит решением класс Counter модуля collections =)
+
