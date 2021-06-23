8f540b908946b0e1946e3284b072b799586269a6
Insolente13 <lanc05@mail.ru>
2021-06-23 21:39:33 +0300

    Module 31 - ok

--- Module31/03_breaking_bad/main.py
+++ Module31/03_breaking_bad/main.py
@@ -16,19 +16,21 @@ season = dict_max_value['season']
 episode = dict_max_value['episode']
 
 dct = {
-"season: ": dict_max_value["season"],
-"episode: ": dict_max_value["episode"],
-"number_of_deaths: ": dict_max_value["number_of_deaths"]
+    "season: ": dict_max_value["season"],
+    "episode: ": dict_max_value["episode"],
+    "number_of_deaths: ": dict_max_value["number_of_deaths"]
 }
 
 for i_dict in res_episodes:
     if i_dict["season"] == str(season) and i_dict["episode"] == str(episode):
         result = {
-                "episode_id": i_dict["episode_id"]
-            }
+            "episode_id": i_dict["episode_id"]
+        }
         dct.update(result)
         break
 
 print(dct)
 with open('Breaking_Bad.json', 'w') as file:
-    json.dump(dct,file,indent=4)
+    json.dump(dct, file, indent=4)
+
+# зачёт!
