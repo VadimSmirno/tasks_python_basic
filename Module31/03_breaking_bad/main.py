b23b612947e7ae5803353923dad1c484091480b7
Insolente13 <lanc05@mail.ru>
2021-06-23 12:52:23 +0300

    Module 31 03, 05, 06 - TODO, another - ok

--- Module31/03_breaking_bad/main.py
+++ Module31/03_breaking_bad/main.py
@@ -11,10 +11,12 @@ req_episodes = requests.get('https://www.breakingbadapi.com/api/episodes')
 res_episodes = json.loads(req_episodes.text)
 # print (json.dumps(res_episodes,indent=4))
 
-dict_max_value = max(res,key=lambda x : x['number_of_deaths'])
-print (json.dumps(dict_max_value,indent=4))
-
+dict_max_value = max(res, key=lambda x: x['number_of_deaths'])
+print(json.dumps(dict_max_value, indent=4))
 
 for i_dict in res_episodes:
     if i_dict["season"] == '2' and i_dict["episode"] == '13':
-        print ("episode_id: ", i_dict["episode_id"])
+        print("episode_id: ", i_dict["episode_id"])
+
+# TODO, после того, как данные собраны, необходимо сохранить их в файл формата json.
+#  Пожалуйста, обратите внимание на условие задания. в нём указано, какие ключи должны присутствовать в словаре =)
