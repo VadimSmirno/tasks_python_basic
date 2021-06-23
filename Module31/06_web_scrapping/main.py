e05cc69058c869eb23fb48e3a9eb1f76a610323a
likhacheva-s <Kapitoxa93!>
2021-06-23 18:32:45 +0300

    Module 31

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -5,7 +5,6 @@ url = 'http://www.columbia.edu/~fdc/sample.html'
 
 my_req = requests.get(url=url)
 res = my_req.text
-# pattern = re.compile('<h3 id=([^>]*)>([^<]*)</h3>')
 print(re.findall(r'<h3 id=".*">(.*)</h3>', res))
 
 # зачёт!
